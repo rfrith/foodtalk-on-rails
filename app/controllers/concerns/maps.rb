cd5e9cdf1af79ed6d8b632f1f6ce9d0fddf1497c
Ryan Frith <rfrith@uga.edu>
2018-02-15 17:05:52 -0500

    Imported maps from legacy site

--- app/controllers/concerns/maps.rb
+++ app/controllers/concerns/maps.rb
@@ -1,11 +1,11 @@
 module Maps
   GA_SNAP_ED_MAPS = [
       {id: 'snap-ed-qualified-areas', target_url: 'https://w-cookie.github.io/SNAP-Ed-Site-Lists/index.html#7/32.706/-83.197', title: "Georgia SNAP-Ed Qualified Areas", description: "" },
-      {id: 'county-health-offices', target_url: 'https://foodtalk.org/sites/default/files/maps_v2/HealthDeptOffice.html', title: "County Health Office Locations", description: "" },
-      {id: 'community-gardens-and-food-deserts', target_url: 'https://foodtalk.org/sites/default/files/maps_v2/CommGardens/CommGardens.html', title: "Community Gardens & Food Deserts", description: "" },
-      {id: 'snap-enrollment-by-zip-code', target_url: 'https://foodtalk.org/sites/default/files/maps_v2/Enrollment_map.html', title: "2010 SNAP Enrollment By Zip Code", description: "" },
-      {id: 'uga-extension-offices', target_url: 'https://foodtalk.org/sites/default/files/maps_v2/Extension_office.html', title: "UGA Extension Offices", description: "" },
-      {id: 'dfacs-offices', target_url: 'https://foodtalk.org/sites/default/files/maps_v2/DFCS_office.html', title: "Division of Family And Children Services Offices", description: "" }
+      {id: 'county-health-offices', target_url: '/maps/HealthDeptOffice.html', title: "County Health Office Locations", description: "" },
+      {id: 'community-gardens-and-food-deserts', target_url: '/maps/CommGardens/CommGardens.html', title: "Community Gardens & Food Deserts", description: "" },
+      {id: 'snap-enrollment-by-zip-code', target_url: '/maps/Enrollment_map.html', title: "2010 SNAP Enrollment By Zip Code", description: "" },
+      {id: 'uga-extension-offices', target_url: '/maps/Extension_office.html', title: "UGA Extension Offices", description: "" },
+      {id: 'dfacs-offices', target_url: '/maps/DFCS_office.html', title: "Division of Family And Children Services Offices", description: "" }
   ]
 
   def self.find_map(id)
