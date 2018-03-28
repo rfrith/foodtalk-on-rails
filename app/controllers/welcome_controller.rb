bc3217bc6acc6ada8f19b8d3c4a72d0908c502f3
Ryan Frith <rfrith@uga.edu>
2018-03-28 14:24:16 -0400

    debugging

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    logger.debug "Rails.application.secrets.blog_feed_url: " + Rails.application.secrets.blog_feed_url
     response = Net::HTTP.get(URI(Rails.application.secrets.blog_feed_url))
     @blogs = JSON.parse(response)
   end
