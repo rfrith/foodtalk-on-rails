a3b5388fcd4b853f2fb2ad948650cb275849b347
Ryan Frith <rfrith@uga.edu>
2018-03-28 14:14:38 -0400

    removed hardcoded wordpress feed URL

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,23 +4,8 @@ class BlogsController < ApplicationController
   require 'json'
 
   def index
-    #TODO: put me in ENV
-    url = 'http://blog.foodtalk.org/blog/wp-json/wp/v2/posts/?_embed'
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
+    response = Net::HTTP.get(URI(Rails.application.secrets.blog_feed_url))
     @blogs = JSON.parse(response)
   end
-
-  def show
-  end
-
-  def demo_blog1
-  end
-
-  def demo_blog2
-  end
-
-  def demo_blog3
-  end
-
+  
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    #TODO: put me in ENV
-    url = 'http://blog.foodtalk.org/blog/wp-json/wp/v2/posts/?_embed'
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
+    response = Net::HTTP.get(URI(Rails.application.secrets.blog_feed_url))
     @blogs = JSON.parse(response)
   end
 end
