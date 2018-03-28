43168ea5f18595e261f4368d6c6e33cd47a9e41b
Ryan Frith <rfrith@uga.edu>
2018-03-28 14:00:59 -0400

    hardcoded wordpress feed URL

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    response = Net::HTTP.get(URI(Rails.application.secrets.blog_feed_url))
+    #TODO: put me in ENV
+    url = 'http://blog.foodtalk.org/blog/wp-json/wp/v2/posts/?_embed'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
     @blogs = JSON.parse(response)
   end
 end
