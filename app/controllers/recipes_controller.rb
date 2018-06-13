f5abaa8d8fd584599b0ec1203b83d2eaa21447c7
Ryan Frith <rfrith@uga.edu>
2018-06-13 10:00:26 -0400

    Fix Recipes Category (Tag) Select

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
       slug = Net::HTTP.get(URI(Rails.application.secrets.blog_feed_url + "tags?slug="+tag))
       parsed_slug = JSON.parse(slug)
       slug_id = parsed_slug[0]["id"]
-      recipes = Net::HTTP.get(URI(Rails.application.secrets.blog_feed_url + "posts/per_page=#{per_page}&?_embed&tags=#{slug_id}"))
+      recipes = Net::HTTP.get(URI(Rails.application.secrets.blog_feed_url + "posts/?per_page=#{per_page}&_embed&tags=#{slug_id}"))
     else
       @all_recipes = true
       slug = Net::HTTP.get(URI(Rails.application.secrets.blog_feed_url + "categories?slug=recipes"))
