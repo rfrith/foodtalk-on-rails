dcb7320b03e4f009eaff43012a11682c5dadbbbf
Ryan Frith <rfrith@uga.edu>
2019-05-22 15:37:21 -0400

    Improvements to mobile views of Home Page, Blogs, Recipes, and Videos; added pagination to Videos list views; caching fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,30 +6,19 @@ class WelcomeController < ApplicationController
   def index
     #TODO: fallback if blog server is down
     begin
+      blogs = get_posts_by_category(3, nil, 1, get_category_slug_id_by_name(RECIPES))
+      @blogs = JSON.parse blogs.body
 
-      recipes_slug = get_cached_api_response('recipes_slug_replies', URI(Rails.application.secrets.blog_feed_url + "categories?slug=recipes")).body
-      parsed_slug = JSON.parse(recipes_slug)
-      recipes_slug_id = parsed_slug[0]["id"]
-
-      #TODO: do this in WordpressUtils (e.g., YoutubeUtils)
-      @blogs ||= JSON.parse get_cached_api_response('wp_blog_replies', URI(Rails.application.secrets.blog_feed_url + "posts/?_embed&per_page=3&categories_exclude=#{recipes_slug_id}")).body
-      @categories ||= JSON.parse get_cached_api_response('wp_blog_category_replies', URI(Rails.application.secrets.blog_feed_url + "categories/?_embed&exclude=#{recipes_slug_id}")).body
-      @recipes ||= JSON.parse get_cached_api_response('wp_recipes_replies', URI(Rails.application.secrets.blog_feed_url + "posts/?_embed&per_page=3&categories=#{recipes_slug_id}")).body
-      @tags ||= JSON.parse get_cached_api_response('wp_tags_replies', URI(Rails.application.secrets.blog_feed_url + "tags")).body
-
+      recipes = get_posts_by_tag(3, nil, 1, nil)
+      @recipes = JSON.parse recipes.body
     rescue Exception => e
       logger.error "An error occurred: #{e.inspect}"
     end
 
     begin
       #get recent videos - TODO: make maxResults & order ENV vars
-
       playlist_id = get_playlist_id({})
-      #playlist_id = Rails.application.secrets.youtube_default_channel
-      #playlist_items_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=#{playlist_id}&maxResults=3&order=date&type=video&key=#{Rails.application.secrets.youtube_api_key}"
-      #@videos ||= JSON.parse get_cached_api_response('yt_recent_videos_replies', URI(playlist_items_url)).body
       @videos ||= get_playlist_items("yt_recent_videos_replies", playlist_id, 3)
-
     rescue Exception => e
       logger.error "An error occurred: #{e.inspect}"
     end
