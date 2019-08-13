bccb8cd71abf72bb90436090f8eeb05da8191176
Ryan Frith <rfrith@uga.edu>
2019-08-13 16:08:26 -0400

    UI Fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,32 +3,43 @@ class WelcomeController < ApplicationController
   include WordpressUtils, WordpressHelper, YoutubeUtils
 
 
-
-
   def index
-    #TODO: fallback if blog server is down
-    begin
-      blogs = get_posts_by_category(3, nil, 1, get_category_slug_id_by_name(RECIPES))
-      @blogs = JSON.parse blogs.body
+
+    if !policy(:site_access).view_default_index?
+
+      redirect_to "/#{request.subdomain}"
+
+    else
+
+      #TODO: fallback if blog server is down
+      begin
+        blogs = get_posts_by_category(3, nil, 1, get_category_slug_id_by_name(RECIPES))
+        @blogs = JSON.parse blogs.body
 
 
-      @tags = get_all_categories_or_tags_as_json(:tags)
-      @categories = filter_categories_or_tags_for_display! get_all_categories_or_tags_as_json(:categories, get_category_slug_id_by_name(RECIPES))
+        @tags = get_all_categories_or_tags_as_json(:tags)
+        @categories = filter_categories_or_tags_for_display! get_all_categories_or_tags_as_json(:categories, get_category_slug_id_by_name(RECIPES))
 
 
-      recipes = get_posts_by_tag(3, nil, 1, nil)
-      @recipes = JSON.parse recipes.body
-    rescue Exception => e
-      logger.error "An error occurred: #{e.inspect}"
+        recipes = get_posts_by_tag(3, nil, 1, nil)
+        @recipes = JSON.parse recipes.body
+      rescue Exception => e
+        logger.error "An error occurred: #{e.inspect}"
+      end
+
+      begin
+        #get recent videos - TODO: make maxResults & order ENV vars
+        playlist_id = get_playlist_id({})
+        @videos ||= get_playlist_items("yt_recent_videos_replies", playlist_id, 3)
+      rescue Exception => e
+        logger.error "An error occurred: #{e.inspect}"
+      end
+
     end
 
-    begin
-      #get recent videos - TODO: make maxResults & order ENV vars
-      playlist_id = get_playlist_id({})
-      @videos ||= get_playlist_items("yt_recent_videos_replies", playlist_id, 3)
-    rescue Exception => e
-      logger.error "An error occurred: #{e.inspect}"
-    end
+
+
+
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
     get 'recipes/:name' => 'recipes#find_by_name', as: 'find_recipe'
     get 'blog/:name' => 'blogs#find_by_name', as: 'find_blog'
     get 'fnv' => 'wordpress_pages#show', slug: 'fnv', show_nav: true
-    get 'caresource' => 'wordpress_pages#show', slug: 'caresource', show_nav: false
+    get 'caresource' => 'wordpress_pages#show', slug: 'caresource', show_nav: true
 
     #internal
     resources :users, only: [:create, :update]
