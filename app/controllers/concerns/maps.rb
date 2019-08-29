245bdae263d479521354369f53a2dceec399743c
Ryan Frith <rfrith@uga.edu>
2019-08-29 13:39:38 -0400

    Added I18N to GIS maps titles

--- app/controllers/concerns/maps.rb
+++ app/controllers/concerns/maps.rb
@@ -3,8 +3,8 @@ module Maps
   extend ActiveSupport::Concern
 
   GA_SNAP_ED_MAPS = [
-      {id: 'snap-ed-eligibility-locator', thumbnail: '/images/snap-ed-eligibility-locator.png', target_url: 'https://jshannon75.github.io/SNAP-Ed-Locator-Web-Tool/index.html', title: "Georgia SNAP-Ed Eligibility Locator", description: "" },
-      {id: 'snap-ed-resources', thumbnail: '/images/snap-ed-resources.png', target_url: 'https://comapuga.shinyapps.io/snaped_resources', title: "SNAP-Ed Resources", description: "" },
+      {id: 'snap-ed-eligibility-locator', thumbnail: '/images/snap-ed-eligibility-locator.png', target_url: 'https://jshannon75.github.io/SNAP-Ed-Locator-Web-Tool/index.html', title: "gis_resources.maps.eligibility_locator", description: "" },
+      {id: 'snap-ed-resources', thumbnail: '/images/snap-ed-resources.png', target_url: 'https://comapuga.shinyapps.io/snaped_resources', title: "gis_resources.maps.snap_ed_resources", description: "" },
   ]
 
   def self.find_map(id)
