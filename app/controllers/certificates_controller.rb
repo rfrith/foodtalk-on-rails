1a17e67c0052bd1439259849e8e1a914bd2bfcd2
Ryan Frith <rfrith@uga.edu>
2019-09-16 18:13:55 -0400

    Certificate: fallback to plain image if any ImageMagick/GhostScript failures occur; removed unused tokens in I18n .yml

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -31,11 +31,11 @@ class CertificatesController < ApplicationController
         better_u_completion_date = curriculum_completion_date(@current_user, LearningModules::BETTER_U)
 
         @certificates = [create_certificate(better_u_image_name, better_u_completion_date), create_certificate(food_etalk_image_name, food_etalk_completion_date)]
+
       end
 
     rescue => e
       logger.error "An error occurred: #{e.inspect}"
-      add_notification :error, t(:error), "#{t("error_occurred")}: #{e.to_s}", false
     end
 
   end
@@ -44,34 +44,41 @@ class CertificatesController < ApplicationController
   private
 
   def create_certificate(image_name, completion_date)
-    #https://stackoverflow.com/a/7625116
-    img = ImageList.new
-    img_url = open(view_context.asset_url(image_name))
-    img.from_blob(img_url.read)
-    txt = Draw.new
-    img.annotate(txt, 0,0,0,20, @current_user.name){
-      txt.gravity = Magick::CenterGravity
-      txt.font_family = 'helvetica'
-      txt.pointsize = 25
-      txt.font_weight = Magick::BoldWeight
-    }
 
-    img.annotate(txt, 0,0,0,130, completion_date){
-      txt.gravity = Magick::CenterGravity
-      txt.font_family = 'helvetica'
-      txt.pointsize = 25
-      txt.font_weight = Magick::BoldWeight
-    }
+    begin
 
-    img.format = 'png'
-    #to send directly to browser, bypassing view
-    #send_data img.to_blob, stream: false, filename: 'certificate.png', type: 'image/png', disposition: 'inline'
-    img_data = Base64.encode64(img.to_blob).gsub(/\n/, "")
+      #https://stackoverflow.com/a/7625116
+      img = ImageList.new
+      img_url = open(view_context.asset_url(image_name))
+      img.from_blob(img_url.read)
+      txt = Draw.new
+      img.annotate(txt, 0,0,0,20, @current_user.name){
+        txt.gravity = Magick::CenterGravity
+        txt.font_family = 'helvetica'
+        txt.pointsize = 25
+        txt.font_weight = Magick::BoldWeight
+      }
+
+      img.annotate(txt, 0,0,0,130, completion_date){
+        txt.gravity = Magick::CenterGravity
+        txt.font_family = 'helvetica'
+        txt.pointsize = 25
+        txt.font_weight = Magick::BoldWeight
+      }
+
+      img.format = 'png'
+      #to send directly to browser, bypassing view
+      #send_data img.to_blob, stream: false, filename: 'certificate.png', type: 'image/png', disposition: 'inline'
+      img_data = Base64.encode64(img.to_blob).gsub(/\n/, "")
+
+    rescue => e
+      #fallback to plain image if any ImageMagick/GhostScript failures occur
+      img = open(view_context.asset_url(image_name))
+      return Base64.encode64 img.read
+    end
 
     return img_data
+
   end
-
-
-
 
 end
