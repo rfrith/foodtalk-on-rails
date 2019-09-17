e6bef3297a2591004f4a436b4d26a9b363a92024
Ryan Frith <rfrith@uga.edu>
2019-09-17 13:56:40 -0400

    Certificate: fallback to plain image if any ImageMagick/GhostScript failures occur

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -43,31 +43,42 @@ class CertificatesController < ApplicationController
   private
 
   def create_certificate(image_name, completion_date)
-    #https://stackoverflow.com/a/7625116
-    img = ImageList.new
-    img_url = open(view_context.asset_url(image_name))
-    img.from_blob(img_url.read)
-    txt = Draw.new
-    img.annotate(txt, 0,0,0,20, @current_user.name){
-      txt.gravity = Magick::CenterGravity
-      txt.font_family = 'helvetica'
-      txt.pointsize = 25
-      txt.font_weight = Magick::BoldWeight
-    }
 
-    img.annotate(txt, 0,0,0,130, completion_date){
-      txt.gravity = Magick::CenterGravity
-      txt.font_family = 'helvetica'
-      txt.pointsize = 25
-      txt.font_weight = Magick::BoldWeight
-    }
+    begin
+      #https://stackoverflow.com/a/7625116
+      img = ImageList.new
+      img_url = open(view_context.asset_url(image_name))
+      img.from_blob(img_url.read)
+      txt = Draw.new
+      img.annotate(txt, 0,0,0,20, @current_user.name){
+        txt.gravity = Magick::CenterGravity
+        txt.font_family = 'helvetica'
+        txt.pointsize = 25
+        txt.font_weight = Magick::BoldWeight
+      }
 
-    img.format = 'png'
-    #to send directly to browser, bypassing view
-    #send_data img.to_blob, stream: false, filename: 'certificate.png', type: 'image/png', disposition: 'inline'
-    img_data = Base64.encode64(img.to_blob).gsub(/\n/, "")
+      img.annotate(txt, 0,0,0,130, completion_date){
+        txt.gravity = Magick::CenterGravity
+        txt.font_family = 'helvetica'
+        txt.pointsize = 25
+        txt.font_weight = Magick::BoldWeight
+      }
+
+      img.format = 'png'
+      #to send directly to browser, bypassing view
+      #send_data img.to_blob, stream: false, filename: 'certificate.png', type: 'image/png', disposition: 'inline'
+      img_data = Base64.encode64(img.to_blob).gsub(/\n/, "")
+
+    rescue => e
+      logger.error "An error occurred: #{e.inspect}"
+      logger.warn "Falling back to render plain certificate: #{image_name}"
+      #fallback to plain image if any ImageMagick/GhostScript failures occur
+      img = open(view_context.asset_url(image_name))
+      return Base64.encode64 img.read
+    end
 
     return img_data
+
   end
 
 end
